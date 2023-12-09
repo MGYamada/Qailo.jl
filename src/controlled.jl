c121e126e9e595ac7b572fc74d525095523583fe
MGYamada <myspinor@gmail.com>
2023-12-09 17:39:03 +0900

    controlled: order was wrong

--- src/controlled.jl
+++ src/controlled.jl
@@ -1,3 +1,11 @@
-CX(i, j, T = ComplexF64) = Gate(reshape(cat(Matrix{T}(I, 2, 2), PauliX(T); dims = (1, 2)), 2, 2, 2, 2), i, j)
-CY(i, j, T = ComplexF64) = Gate(reshape(cat(Matrix{T}(I, 2, 2), PauliY(T); dims = (1, 2)), 2, 2, 2, 2), i, j)
-CZ(i, j, T = ComplexF64) = Gate(reshape(cat(Matrix{T}(I, 2, 2), PauliZ(T); dims = (1, 2)), 2, 2, 2, 2), i, j)
+function controlled(u::Matrix{T}) where T
+    i, j = size(u)
+    permutedims(reshape(cat(Matrix{T}(I, i, j), u; dims = (1, 2)), i, 2, j, 2), [2, 1, 4, 3])
+end
+
+CX(i, j, T = ComplexF64) = Gate(controlled(PauliX(T)), i, j)
+CY(i, j, T = ComplexF64) = Gate(controlled(PauliY(T)), i, j)
+CZ(i, j, T = ComplexF64) = Gate(controlled(PauliZ(T)), i, j)
+CP(phi, i, j, T = ComplexF64) = Gate(controlled(Phase(phi, T)), i, j)
+
+CCX(i, j, k, T = ComplexF64) = Gate(reshape(controlled(reshape(controlled(PauliX(T)), 4, 4)), 2, 2, 2, 2, 2, 2), i, j, k) # fix later
