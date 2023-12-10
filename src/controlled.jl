d0844eb757f4f8a27a03d3d8df6a7db7a2be9132
MGYamada <myspinor@gmail.com>
2023-12-10 10:15:38 +0900

    Phase gate fixed, adjoint

--- src/controlled.jl
+++ src/controlled.jl
@@ -6,6 +6,6 @@ end
 CX(i, j, T = ComplexF64) = Gate(controlled(PauliX(T)), i, j)
 CY(i, j, T = ComplexF64) = Gate(controlled(PauliY(T)), i, j)
 CZ(i, j, T = ComplexF64) = Gate(controlled(PauliZ(T)), i, j)
-CP(phi, i, j, T = ComplexF64) = Gate(controlled(Phase(phi, T)), i, j)
+CP(phi::T, i, j, S = ComplexF64) where T <: Real = Gate(controlled(PhaseP(convert(S, phi))), i, j)
 
 CCX(i, j, k, T = ComplexF64) = Gate(reshape(controlled(reshape(controlled(PauliX(T)), 4, 4)), 2, 2, 2, 2, 2, 2), i, j, k) # fix later
